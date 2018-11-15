75d3d7ec77376702ea407d00476fd41df0beeb91
khym24 <44380842+khym24@users.noreply.github.com>
2018-11-15 17:36:56 -0500

    Update LED_Diagnostics.h

--- LED_Diagnostics.h
+++ LED_Diagnostics.h
@@ -6,10 +6,14 @@ class LED_Diagnostics
 {
 public:
   void statusCheck(int fix);
+  // led object takes parameters that indicate the readiness of:
+  // GPS satellite & communication: fix
+  // BMP: wip...
+  // SD: ...
   
 private:
-  boolean CHECK_BMP(void);
+  boolean CHECK_BMP(void);    // check functions, wip, might not even need
   boolean SD_CHECK(void);
-  void displayLED(void);
+  void displayLED(void);      // outputs the colours
   
 }
